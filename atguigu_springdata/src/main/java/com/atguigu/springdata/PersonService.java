a10022e3060e897b06f6beda16dba99500695428
cyhbyw <swust2009cyh@qq.com>
2017-11-20 17:58:37 +0800

    initial import

---
+++ atguigu_springdata/src/main/java/com/atguigu/springdata/PersonService.java
@@ -0,0 +1,27 @@
+package com.atguigu.springdata;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Administrator
+ */
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepsotory personRepsotory;
+
+    @Transactional(rollbackFor = {Exception.class, Error.class})
+    public void savePersons(List<Person> persons) {
+        personRepsotory.save(persons);
+    }
+
+    @Transactional(rollbackFor = {Exception.class, Error.class})
+    public void updatePersonEmail(String email, Integer id) {
+        personRepsotory.updatePersonEmail(id, email);
+    }
+}
